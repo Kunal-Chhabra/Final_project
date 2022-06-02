1ef3c71934053a18a792979d3a4d7c1fd70c1e6a
Kunal-Chhabra <105283137+Kunal-Chhabra@users.noreply.github.com>
2022-06-02 10:28:51 +0530

    Add files via upload

---
+++ Final Project/CloudOn/CloudOn/Controllers/UserRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace CloudOn.Controllers
+{
+    internal class UserRequest
+    {
+    }
+}
---
+++ Final Project/CloudOn/CloudOn/RequestModel/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudOn.RequestModel
+{
+    public class LoginModel
+
+    {
+        internal object password;
+    }
+}
